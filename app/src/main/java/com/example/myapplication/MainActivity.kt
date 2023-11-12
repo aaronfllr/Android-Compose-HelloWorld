a301e213ec0dc99dd2cd77b4bd7e00412799638f
aaron <45025327+aaronfllr@users.noreply.github.com>
2023-11-12 17:24:50 -0500

    Commit Project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            }
+                        )
+                        Button(onClick = {
+                            if(name.isNotBlank()) {
+                                names = names + name
+                            }
+                        }) {
+                            Text(text = "Add")
+                        }
+                    }  
+                    LazyColumn {
+                        items(names) {currentName ->
+                            Text(text = currentName)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
