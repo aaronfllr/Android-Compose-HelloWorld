df9aacd3c7c2cd4254a8f2a024edd6d46bceba77
aaron <45025327+aaronfllr@users.noreply.github.com>
2023-11-13 12:15:18 -0500

    Commit Project

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,15 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -21,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 
 class MainActivity : ComponentActivity() {
@@ -47,25 +52,40 @@ class MainActivity : ComponentActivity() {
                             value = name,
                             onValueChange = { text ->
                                 name = text
-                            }
+                            },
+                            modifier = Modifier.weight(1f)
                         )
+                        Spacer(modifier = Modifier.width(16.dp))
                         Button(onClick = {
                             if(name.isNotBlank()) {
                                 names = names + name
+                                name = ""
                             }
                         }) {
                             Text(text = "Add")
                         }
                     }  
-                    LazyColumn {
-                        items(names) {currentName ->
-                            Text(text = currentName)
-                        }
-                    }
+                    NameList(names = names)
                 }
             }
         }
     }
 }
 
-
+@Composable
+fun NameList(
+    names: List <String>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn {
+        items(names) {currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
+        }
+    }
+}
